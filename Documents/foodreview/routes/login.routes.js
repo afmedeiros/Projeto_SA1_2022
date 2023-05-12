77179906d8bd2d1828dac45bd0b9666c294e0696
ARTHUR FELIPE MEDEIROS <arthur_f_medeiros@estudante.sesisenai.org.br>
2023-05-11 21:36:53 -0300

    integrando back e front

--- Documents/foodreview/routes/login.routes.js
+++ Documents/foodreview/routes/login.routes.js
@@ -1,7 +1,64 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
+import User from '../models/User.js'
+import jwt from 'jsonwebtoken';
 
 const login = express.Router();
 
-login.get('/', (req, res) => res.send("Rota de Login"));
+//leva a rota de login
+login.post('/', async (req, res) => {
 
-export default login;
+    // Recebe as informações de LOGIN
+    const { email, password } = req.body;
+
+    // Buscar EMAIL no BD, se existente, e armazenar
+    const registeredUser = await User.findOne(
+        {where: {email}}
+    ).catch(
+        (err) => {
+            console.log("Error", err);
+        }
+    )
+
+        
+    // Caso EMAIL inexistente, informar o usuário
+    if (!registeredUser) {
+        return res
+            .status(400)
+            .json({message: "Email ou Senha inválidos."})
+    };
+    
+    // Caso EMAIL existente, verificar a SENHA do usuário
+    if (!bcrypt.compareSync( password, registeredUser.password)) {
+        return res
+            .status(400)
+            .json({message: "Email ou Senha inválidos."})
+    }
+
+    // Caso SENHA correta, gerar TOKEN de acesso
+    const token = jwt.sign(
+        //Payload: o que será armazenado no TOKEN
+        {
+            id: registeredUser.id,
+            name: registeredUser.name,
+            admin: registeredUser.admin
+        },
+        // Secret or private key
+        process.env.JWT_SECRET,
+        // Options (expiração em 1 hora)
+        {
+            expiresIn: '1h'
+        }
+        
+    )
+
+    res.json(
+        {
+            message: "Bem-Vindo",
+            token: token
+        }
+    )
+
+    }
+);
+export default login
